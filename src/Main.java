f51ef575f1d67f127d8eaf2e886946d9c01a71da
Rewansh57 <rewanshchoudhary57@gmail.com>
2024-09-15 19:06:51 +0530

    binary search algorithm with crlf ignore thing

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        int [] array=new int [10000000];
+        for(int i=0;i<array.length;i++)
+        {
+        array[i]=i;
+        }
+       int index= binary_search(76,array);
+
+        if (index==-1){
+            System.out.println("The elemnt not found");
+        }
+        else{
+            System.out.println("The element found "+ index);
+        }
+      }
+    private static  int binary_search(int target,int [] array){
+        int low=0;
+        int high=array.length-1;
+
+
+        while(low<=high){
+            int middle=low+(high-low)/2;
+            int value=array[middle];
+            System.out.println("Middle "+value);
+            if (value>target){
+                high=middle-1;
+            }
+            else if(value<target){
+                low=middle+1;
+            }
+            else{
+                return middle;
+            }
+
+
+
+
+
+
+        }
+        return -1;
+    }}
